64afe3706c1ba7cff4525566afa948d50ad9a353
xd009642 <danielmckenna93@gmail.com>
2018-07-29 22:41:50 +0100

    First (incorrect) HDL coregen
    Just hacked something out and got the HDL to generate successfully. It also matches what I expected!
    
    Now to properly implement CORDIC not just bits of it.

--- coregen/cordic_gen.py
+++ coregen/cordic_gen.py
@@ -1,24 +1,45 @@
 import argparse
 from myhdl import *
+from math import atan2, pi
+
 
 @block
-def cordic_gen(phasebits, stages, phases):
-    raise NotImplemented
+def cordic_gen(clk, rst, phasebits, stages):
+
+    phases = [atan2(1, 2**i) for i in range(stages)]
+    phases = [x * (4.0 * (1<<(phasebits-2)))/(2.0*pi) for x in phases]
+    # No floats in FPGA land. Keeping the old floats in case I can create comments
+    # from them in future for generated VHDL
+    int_phases = tuple([int(x) for x in phases])
+    # 360/45 = 8 so if we want to split into 45 degree blocks only need 3 bits
+    # to represent a block
+    
+    x = Signal(intbv(0)[args.stages:])
+    y = Signal(intbv(0)[args.stages:])
+    z = Signal(intbv(0)[args.stages:])
+    @always(clk.posedge)
+    def update():
+        for i in range(stages):
+            x[i] = int_phases[i]
+
+    return update
 
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--stages', '-s', help='Number of stages in the CORDIC processor', type=int)
-parser.add_argument('--phasebits', '-p', help='Number of bits used to store phase values', type=int)
+parser.add_argument('--stages', '-s', help='Number of stages in the CORDIC processor', type=int, default = 5)
+parser.add_argument('--phasebits', '-p', help='Number of bits used to store phase values', type=int, default = 15)
 parser.add_argument('--out', '-o', help='Output filename', type=str, default='CORDIC.vhd')
 
 args = parser.parse_args()
 
 print(args)
+clk = Signal(bool(0))
+rst = Signal(bool(0))
 pb = Signal(intbv(0)[args.phasebits:])
 s = Signal(intbv(0)[args.stages:])
 
-concrete_cordic = cordic_gen(pb, s, [])
+concrete_cordic = cordic_gen(clk, rst, args.phasebits, args.stages)
 
 concrete_cordic.convert(hdl='VHDL')
 
