d8e1ecc64275e1a20bd9a72083e234898049752b
xd009642 <danielmckenna93@gmail.com>
2018-08-14 21:28:21 +0100

    Switched the verilog!

---
+++ src/audio_interface.v
@@ -0,0 +1,22 @@
+module audio_interface(
+clk
+rst
+phase_out)
+
+input clk;
+input rst;
+output [15:0] phase_out;
+
+reg [15:0] phase_out
+
+// For now just generate a saw wave.
+always @ (posedge clk)
+begin: wave
+    if(reset == 1'b1) begin
+        phase_out <= 0;
+    end 
+    else begin
+        phase_out <= #1 phase_out + 1;
+    end
+end
+
